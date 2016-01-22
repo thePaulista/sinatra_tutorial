8ca603dbe5d2a7ae8c17623df5341b666079a8d6
Hedy <hedywoo@gmail.com>
2016-01-22 10:32:42 -0700

    guess high or low functionality

--- web_guesse1.rb
+++ web_guesse1.rb
@@ -5,5 +5,16 @@ require 'sinatra/reloader'
 number = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+
+  if guess < number
+    message = "Too low, guess again:"
+  elsif guess == number
+    message = "you got it!"
+  else
+    message ="Too high, guess again"
+  end
+
+  erb :index, :locals => {:number => number, :message => message}
+
 end
