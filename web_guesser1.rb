aee8db1dbbe466fad633068a9e03d3695ba02100
Hedy <hedywoo@gmail.com>
2016-01-24 14:33:43 -0600

    currently adding stuff from Friday. Sick in bed for two days :-(

--- web_guesser1.rb
+++ web_guesser1.rb
@@ -3,23 +3,32 @@ require 'sinatra/reloader'
 require 'pry'
 
 SECRET_NUMBER = rand(100)
+DISPLAY_MSSG = "The secret number is #{SECRET_NUMBER}"
+# @color = pink
 
 get '/' do
-
   guess = params['guess'].to_i
+  message = check_guess(guess)
 
-  if guess < SECRET_NUMBER - 5
-    message = "Way way too low. Muy bajo senor/a!"
-  elsif guess < SECRET_NUMBER
-    message = "Almost there. Go up a wee bit!"
-  elsif guess > SECRET_NUMBER + 5
-    message = "Too cold, and too high. Try again!"
-  elsif guess > SECRET_NUMBER
-      messsage = "Close! But its still high"
-  else
-    message == "Yay!"
-  end
-
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
-#
+  erb :index, :locals => {:number  => SECRET_NUMBER,
+                          :message => message,
+                          :display_message => DISPLAY_MSSG
+                          # :color => @color
+                        }
 end
+
+  def check_guess(guess)
+    if guess < SECRET_NUMBER - 5
+      message = "Way way too low. Muy bajo senor!"
+      # @color = pink
+    elsif guess < SECRET_NUMBER
+      message = "Almost there. Go up a wee bit!"
+    elsif guess > SECRET_NUMBER + 5
+      message = "Too cold, and too high. Try again!"
+    elsif guess > SECRET_NUMBER
+      messsage = "Close! But its still high"
+    else
+      message = "Yay!"
+      DISPLAY_MSSG
+    end
+  end
