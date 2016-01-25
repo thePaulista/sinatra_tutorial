c0e901eded68aa96b02b4b4d93bad64ee1c34b4a
Hedy <hedywoo@gmail.com>
2016-01-25 00:30:13 -0600

    finished I4 - technicolor

--- web_guesser1.rb
+++ web_guesser1.rb
@@ -3,8 +3,7 @@ require 'sinatra/reloader'
 require 'pry'
 
 SECRET_NUMBER = rand(100)
-DISPLAY_MSSG = "The secret number is #{SECRET_NUMBER}"
-# @color = pink
+@color = "background:#FBEFF2"
 
 get '/' do
   guess = params['guess'].to_i
@@ -12,23 +11,32 @@ get '/' do
 
   erb :index, :locals => {:number  => SECRET_NUMBER,
                           :message => message,
-                          :display_message => DISPLAY_MSSG
-                          # :color => @color
-                        }
+                          :color => @color
+                          }
 end
 
   def check_guess(guess)
     if guess < SECRET_NUMBER - 5
+      @color = "background: #F2F5A9"
       message = "Way way too low. Muy bajo senor!"
-      # @color = pink
     elsif guess < SECRET_NUMBER
+      @color = "background: #FE9A2E"
       message = "Almost there. Go up a wee bit!"
     elsif guess > SECRET_NUMBER + 5
+      @color = "background: #AEB404"
       message = "Too cold, and too high. Try again!"
     elsif guess > SECRET_NUMBER
+      @color = "background: #BE81F7"
       messsage = "Close! But its still high"
+    elsif guess = NAN
+      message = "Numbers only please"
     else
-      message = "Yay!"
-      DISPLAY_MSSG
+      @color = "background: #FA58D0"
+      message = "Yup! The SECRET NUMBER was #{SECRET_NUMBER}"
     end
   end
+
+
+  # <!-- <p>
+  # "<%= display_message %>"
+  # </p> -->
