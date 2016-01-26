aa5ff925951cef33a09dbf89b6461571618db0d3
Hedy <hedywoo@gmail.com>
2016-01-26 00:11:04 -0600

    added the count extension

--- web_guesser1.rb
+++ web_guesser1.rb
@@ -2,39 +2,55 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
+@@count = 6
 SECRET_NUMBER = rand(100)
 @color = "background:#FBEFF2"
 
 get '/' do
-  guess = params['guess'].to_i
-  message = check_guess(guess)
-
-  erb :index, :locals => {:number  => SECRET_NUMBER,
-                          :message => message,
-                          :color => @color
-                          }
+guess = params['guess'].to_i
+count
+message = check_guess(guess)
+# count
+erb :index, :locals => {:number  => SECRET_NUMBER,
+                        :message => message,
+                        :color => @color
+                        }
 end
 
-  def check_guess(guess)
-    if guess < SECRET_NUMBER - 5
-      @color = "background: #F2F5A9"
-      message = "Way way too low. Muy bajo senor!"
-    elsif guess < SECRET_NUMBER
-      @color = "background: #FE9A2E"
-      message = "Almost there. Go up a wee bit!"
-    elsif guess > SECRET_NUMBER + 5
-      @color = "background: #AEB404"
-      message = "Too cold, and too high. Try again!"
-    elsif guess > SECRET_NUMBER
-      @color = "background: #BE81F7"
-      messsage = "Close! But its still high"
-    elsif guess = NAN
-      message = "Numbers only please"
-    else
-      @color = "background: #FA58D0"
-      message = "Yup! The SECRET NUMBER was #{SECRET_NUMBER}"
-    end
+ def count
+   if @@count == 0
+     message = "Restart game with 5 new tries"
+     restart_game
+   else
+     @@count -= 1
+   end
+ end
+
+ def restart_game
+   SECRET_NUMBER
+   @@count = 5
+ end
+
+
+def check_guess(guess)
+  if guess < SECRET_NUMBER - 5
+    @color = "background: #F2F5A9"
+    message = "Way way too low. Muy bajo senor!"
+  elsif guess < SECRET_NUMBER
+    @color = "background: #FE9A2E"
+    message = "Almost there. Go up a wee bit!"
+  elsif guess > SECRET_NUMBER + 5
+    @color = "background: #AEB404"
+    message = "Too cold, and too high. Try again!"
+  elsif guess > SECRET_NUMBER
+    @color = "background: #BE81F7"
+    messsage = "Close! But its still high"
+  else
+    @color = "background: #FA58D0"
+    message = "Yup! The SECRET NUMBER was #{SECRET_NUMBER}"
+    restart_game
   end
+end
 
 
   # <!-- <p>
